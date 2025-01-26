3d11c17157593b5cf89a70c6a2197cc3da948464
Arafat-029 <arafathassane29@gmail.com>
2025-01-26 19:50:05 +0100

    test

--- src/presentation/BankAppMenus.java
+++ src/presentation/BankAppMenus.java
@@ -21,7 +21,7 @@ public class BankAppMenus {
 
     public void showLoginMenu() {
         while (true) {
-            System.out.println("=== Login ===");
+            System.out.println("=== Welcome to OnlineBank ===");
             System.out.println("1. Log in");
             System.out.println("2. Exit application");
             System.out.print("Choose an option: ");
